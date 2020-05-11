d5699de3f6e9ecd2fc26b15490b2a4877eb7237c
tmaulon <tmaulon@betomorrow.com>
2020-05-11 17:51:40 +0200

    improve building template page with plans

--- src/pages/building-template-page.tsx
+++ src/pages/building-template-page.tsx
@@ -1,65 +1,82 @@
-import React, { useContext, useState, useEffect } from "react"
+import React, { useState, useEffect } from "react"
 import "../App.css"
-import { BuildingsZonesSection } from "../components/building-zones/buildings-zones-section"
-import { IBuilding, BuildingCard } from "../components/building-card/building-card"
 import styled from "styled-components"
 import { Container } from "../components/container/container"
-import { RouteComponentProps } from "react-router-dom"
-import { FakeBuildingsData } from "./home-page"
 import { BuildingCardDetails } from "../components/building-card/building-card-details"
-
-interface BuildingMatchParams {
-	buildingId: string
-}
-interface BuildingProps extends RouteComponentProps<BuildingMatchParams> {
-	building: IBuilding
-}
+import { useCustomfetch } from "../hooks/useCustomFetch"
+import { FakeBuildingsData } from "../content/fake-buildings-data"
+import { BuildingProps, IBuilding } from "../domain/building"
 
 export const BuildingTemplatePage: React.FC<BuildingProps> = ({ match }) => {
-	const [fakeUserId, setFakeUserId] = useState<number>()
 	const fakeBuildings: IBuilding[] = FakeBuildingsData
 	const [fakeBuilding, setFakeBuilding] = useState<IBuilding>()
-	const [userData, setUserData] = useState()
+	const [url, setUrl] = useState<string>("")
+	const { data, loading, error } = useCustomfetch(url)
+
+	const getUserData = (userId: number) => {
+		//delete + 1 when back is done
+		setUrl(`/demo/user/${userId + 1}`)
+	}
 
 	useEffect(() => {
 		setFakeBuilding(fakeBuildings.find(({ id }) => id === parseInt(match.params.buildingId)))
-	}, [])
+	}, [fakeBuildings, match.params.buildingId])
 	useEffect(() => {
-		setFakeUserId(parseInt(match.params.buildingId))
-		fetch(`/demo/user/${parseInt(match.params.buildingId)}`)
-			.then((res) => res.json())
-			.then((json) => {
-				console.log("json", json)
-				setUserData(json)
-			})
-	}, [])
+		getUserData(parseInt(match.params.buildingId))
+	})
 
 	return (
 		<>
-			<PresentationSection>
-				<Container>
-					<PictureWrapper>
-						<BuildingPicture
-							src={`${
-								fakeBuilding?.picture && fakeBuilding?.picture.src
-									? process.env.PUBLIC_URL + fakeBuilding?.picture.src
-									: process.env.PUBLIC_URL + "/images/buildings/batiment-placeholder.png"
-							}`}
-							alt={`${
-								fakeBuilding?.picture && fakeBuilding?.picture.alt ? fakeBuilding?.picture.alt : "Image du Bâtiment"
-							}`}
-						/>
-					</PictureWrapper>
-					<div>
-						<h1>Building-{match.params.buildingId}</h1>
-						<p>Welcome in the Building-{match.params.buildingId} page</p>
-						{fakeBuilding && <BuildingCardDetails building={fakeBuilding}></BuildingCardDetails>}
-					</div>
-				</Container>
-			</PresentationSection>
+			{fakeBuilding && (
+				<PresentationSection>
+					<Container>
+						<PictureWrapper>
+							<BuildingPicture
+								src={`${
+									fakeBuilding.picture && fakeBuilding.picture.src
+										? process.env.PUBLIC_URL + fakeBuilding.picture.src
+										: process.env.PUBLIC_URL + "/images/buildings/batiment-placeholder.png"
+								}`}
+								alt={`${
+									fakeBuilding.picture && fakeBuilding.picture.alt ? fakeBuilding.picture.alt : "Image du Bâtiment"
+								}`}
+							/>
+						</PictureWrapper>
+						<BuildingInformations>
+							<h1>{fakeBuilding.name}</h1>
+							<p>Bienvenu dans le bâtiment : {fakeBuilding.name}</p>
+							<BuildingCardDetails building={fakeBuilding}></BuildingCardDetails>
+						</BuildingInformations>
+						<FloorsPlansWrapper>
+							<h2>Plus d'informations sur les plans du bâtiment</h2>
+							<p>
+								Sélectionne un étage pour en savoir pus sur son aménagement et les cllaborateurs qui y sont installés
+							</p>
+							<PlansList>
+								{fakeBuilding &&
+									fakeBuilding.plans.map((p, i) => (
+										<PlanItem key={`${p.id}-${i}`}>
+											<img
+												src={`${p.picture ? p.picture.src : ""}`}
+												alt={`${p.picture ? p.picture.alt : `Plan du ${p.name}`}`}
+											/>
+											<PlanTitle>{p.name}</PlanTitle>
+										</PlanItem>
+									))}
+							</PlansList>
+						</FloorsPlansWrapper>
+					</Container>
+				</PresentationSection>
+			)}
 			<section>
 				<h2>Fetch All User data</h2>
-				<pre>{JSON.stringify(userData, null, 2)}</pre>
+				{loading && url && (
+					<LoadingWrapper>
+						<p>Loading ...</p>
+					</LoadingWrapper>
+				)}
+				{!loading && data && <pre>{JSON.stringify(data, null, 2)}</pre>}
+				{!loading && error && <p>{error}</p>}
 			</section>
 		</>
 	)
@@ -78,7 +95,10 @@ const PresentationSection = styled.section`
 		display: grid;
 		grid-template-columns: 1fr;
 		grid-auto-rows: auto;
-		grid-template-rows: minmax(80vh, 1fr);
+		grid-template-rows: repeat(2, 1fr);
+		grid-template-areas:
+			"BuildingPicture BuildingInformations"
+			"FloorsPlansWrapper FloorsPlansWrapper";
 		gap: 50px;
 		align-items: center;
 		@media screen and (min-width: 768px) {
@@ -87,6 +107,9 @@ const PresentationSection = styled.section`
 	}
 `
 const PictureWrapper = styled.div`
+	grid-area: BuildingPicture;
+	grid-column: span 1;
+	grid-row: span 1;
 	height: 100%;
 `
 const BuildingPicture = styled.img`
@@ -94,4 +117,34 @@ const BuildingPicture = styled.img`
 	width: 100%;
 	object-fit: contain;
 	object-position: center;
+`
+const LoadingWrapper = styled.div`
+	margin-left: 10px;
+`
+const BuildingInformations = styled.div`
+	grid-area: BuildingInformations;
+	grid-column: span 1;
+	grid-row: span 1;
+`
+const FloorsPlansWrapper = styled.div`
+	grid-area: FloorsPlanWrapper;
+	grid-column: span 2;
+	grid-row: span 1;
+`
+const PlansList = styled.div`
+	display: grid;
+	grid-template-columns: repeat(2, 1fr);
+	grid-template-rows: 1fr;
+	grid-auto-rows: 1fr;
+`
+const PlanItem = styled.div`
+	width: 100%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+`
+const PlanTitle = styled.h3`
+	font-size: 1rem;
+	color: #00b0bd;
 `
